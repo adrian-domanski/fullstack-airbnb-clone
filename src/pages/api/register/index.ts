b8b0b0a1b9f32e7a2e517cacbcc9e63626f9fa1a
Adrian Domański <adrian.domanski.dev@gmail.com>
2023-05-12 11:43:10 +0200

    feat: add /api/register for creating user

---
+++ src/pages/api/register/index.ts
@@ -0,0 +1,25 @@
+import bcrypt from 'bcrypt'
+import prisma from '../../../libs/prismadb'
+import { NextApiRequest, NextApiResponse } from 'next'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method === 'POST') {
+    const body = await req.body
+    const { email, name, password } = body
+
+    const hashedPassword = await bcrypt.hash(password, 12)
+
+    const user = await prisma.user.create({
+      data: {
+        email,
+        name,
+        hashedPassword,
+      },
+    })
+
+    return res.json(user)
+  }
+}
