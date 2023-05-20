2898bb031c3b3ffd1f5feb985f80efcfbdabe3d1
Adrian Domański <adrian.domanski.dev@gmail.com>
2023-05-20 17:23:47 +0200

    feat: add protected routes

--- src/components/Layout/Navbar/Search/Search.tsx
+++ src/components/Layout/Navbar/Search/Search.tsx
@@ -2,17 +2,60 @@ import { BiSearch } from 'react-icons/bi'
 
 import * as Styled from './Search.styles'
 import useSearchModal from '../../../../hooks/useSearchModal'
+import { useSearchParams } from 'next/navigation'
+import useCountries from '../../../../hooks/useCountries'
+import { useMemo } from 'react'
+import { differenceInDays } from 'date-fns'
 
 const Search = () => {
   const searchModal = useSearchModal()
+  const params = useSearchParams()
+  const { getByValue } = useCountries()
+
+  const locationValue = params?.get('locationValue')
+  const startDate = params?.get('startDate')
+  const endDate = params?.get('endDate')
+  const guestCount = params?.get('guestCount')
+
+  const locationLabel = useMemo(() => {
+    if (locationValue) {
+      return getByValue(locationValue as string)?.label
+    }
+
+    return 'Anywhere'
+  }, [locationValue, getByValue])
+
+  const durationLabel = useMemo(() => {
+    if (startDate && endDate) {
+      const start = new Date(startDate as string)
+      const end = new Date(endDate as string)
+      let diff = differenceInDays(end, start)
+
+      if (diff === 0) {
+        diff = 1
+      }
+
+      return `${diff} Days`
+    }
+
+    return 'Any Week'
+  }, [startDate, endDate])
+
+  const guestLabel = useMemo(() => {
+    if (guestCount) {
+      return `${guestCount} Guests`
+    }
+
+    return 'Add Guests'
+  }, [guestCount])
 
   return (
     <Styled.Wrapper onClick={searchModal.onOpen}>
       <Styled.Content>
-        <Styled.LocationLabel>Location Label</Styled.LocationLabel>
-        <Styled.DurationLabel>Duration Label</Styled.DurationLabel>
+        <Styled.LocationLabel>{locationLabel}</Styled.LocationLabel>
+        <Styled.DurationLabel>{durationLabel}</Styled.DurationLabel>
         <Styled.GuestLabelWrapper>
-          <Styled.GuestLabel>Guest label</Styled.GuestLabel>
+          <Styled.GuestLabel>{guestLabel}</Styled.GuestLabel>
           <Styled.SearchWrapper>
             <BiSearch size={18} />
           </Styled.SearchWrapper>
